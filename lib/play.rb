0048bd8f9cb2f4d49c5a936f5b61864a529e4c44
Heath Mast <hrmast@gmail.com>
2020-03-18 05:53:37 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,8 +38,8 @@ end
 # Define your play method below
 def play(board)
   counter = 0
-  while counter <= 9
-    counter = counter + 1
+  while counter < 10
     turn(board)
+    counter += 1
   end
 end
